8186c7e8c00597c17f2b87758fdf9de78d6f7fe5
EfrainBM <145635254+EfrainBM@users.noreply.github.com>
2025-05-19 11:03:09 -0600

    Update mainE.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_URL = "http://44.199.198.188:5000/api/devices";
+const API_URL = "http://34.205.166.69:5000/api/devices";
 
 // Variable global para guardar la IP pública
 let ipPublica = "";
